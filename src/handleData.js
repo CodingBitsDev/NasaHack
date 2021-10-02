9d3912b66b0da263c89e29e652853f778aad25f4
Nick <nick.conrad@dfki.de>
2021-10-02 14:06:08 +0200

    Sorry

--- src/handleData.js
+++ src/handleData.js
@@ -1,13 +1,13 @@
 export function handleData(){
 	
-	let fits_callback = function ()	{
-		console.log(this)
-	}
+//	let fits_callback = function ()	{
+//		console.log(this)
+//	}
 
-	let FITS = window.astro.FITS;
-	let fits = new FITS("../data/NEOS_SCI_2021001004704.fits", fits_callback)
+//	let FITS = window.astro.FITS;
+//	let fits = new FITS("../data/NEOS_SCI_2021001004704.fits", fits_callback)
 	
-	console.log(typeof(FITS))
-	console.log(typeof(fits))
+//	console.log(typeof(FITS))
+//	console.log(typeof(fits))
 	console.log("Hello World")
 }
