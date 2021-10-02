d45731a55f4146672fec09226c04eb32dde180b5
Nils Rohr <nils.rohr@zreality.com>
2021-10-02 15:54:19 +0200

    removed fits

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import { useEffect } from 'react';
 import './App.scss';
 import BabylonCanvas from './BabylonCanvas';
-import {handleData} from './handleData';
 import("@babylonjs/core/Debug/debugLayer")
 import('@babylonjs/inspector')
 
 
 function App() {
-  useEffect(() => {
-    handleData();
-  });
-
   return (
     <div className="App">
       <BabylonCanvas/>
