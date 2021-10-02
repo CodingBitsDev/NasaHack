73033cb7a3e76312ac958e5eb43e89dd9ea63f29
Jens Kolbenschlag <jens.software@gmail.com>
2021-10-02 16:53:11 +0200

    fixed coordinate system

--- src/renderOrbit.js
+++ src/renderOrbit.js
@@ -5,14 +5,20 @@ import * as satellite from 'satellite.js';
 let renderInterval = null;
 
 export function updateOrbit(orbit) {
-    let delta = Date.now() / (60 * 1000) - 27219648;
     let res = orbit.options.points.length;
+    var time = new Date();
+    var gmst = satellite.gstime(time);
+    let test = satellite.eciToGeodetic(satellite.propagate(orbit.satrec, time).position);
+    //console.log(test);
+    //console.log(satellite.degreesLong(test.longitude), satellite.degreesLat(test.latitude));
     for (let i = 0; i < res; i++){
-        let pos = satellite.sgp4(orbit.satrec, i / 10 + delta).position;
+        
+        let pos = satellite.eciToEcf(satellite.propagate(orbit.satrec, time).position, gmst);
         let point = orbit.options.points[i];
-        point.x = pos.x / 100;
-        point.y = pos.y / 100;
-        point.z = pos.z / 100;
+        point.x = pos.y / 100;
+        point.y = pos.z / 100;
+        point.z = -pos.x / 100;
+        time.setSeconds(time.getSeconds() - 30);
     }
 
     orbit.options.instance = BABYLON.MeshBuilder.CreateLines("lines", orbit.options);
@@ -20,8 +26,10 @@ export function updateOrbit(orbit) {
 
 export default function createOrbit(scene, tle, color){
     // Sample TLE
-    var tleLine1 = '1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927',
-    tleLine2 = '2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537';  
+    var tleLine1 = '1 25544U 98067A   21275.52277778  .00006056  00000-0  11838-3 0  9993',
+    tleLine2 = '2 25544  51.6451 172.0044 0004138  50.9000 316.9051 15.48905523305232';  
+    
+
 
     // Initialize a satellite record
     var satrec = satellite.twoline2satrec(tleLine1, tleLine2);
@@ -32,7 +40,7 @@ export default function createOrbit(scene, tle, color){
     const res = 100;
     for (let i = 0; i < res; i++){
         points.push(new BABYLON.Vector3(0, 0, 0));
-        colors.push(new BABYLON.Color4(1, 1, i / res, i / res));
+        colors.push(new BABYLON.Color4(1, 1, (res - i), (res - i) / res));
     }
   
     const options = {
