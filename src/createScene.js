0f3bef7a60cc079a7b762e50983a7f4f97a3d6d9
Jens Kolbenschlag <jens.software@gmail.com>
2021-10-02 21:38:40 +0200

    colision detection

--- src/createScene.js
+++ src/createScene.js
@@ -2,8 +2,11 @@ import * as BABYLON from '@babylonjs/core/Legacy/legacy';
 import * as GUI from '@babylonjs/gui';
 
 import renderTrash from './renderTrash';
+import checkCollision from './checkCollision';
 import Earth from './BabylonComponents/Earth';
 import Axis from './BabylonComponents/Axis';
+
+let trash;
 
 export function createScene (engine, canvas) {
   let scene = new BABYLON.Scene(engine);
@@ -44,15 +47,15 @@ export function createScene (engine, canvas) {
 	// const camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene);
 	const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI/2, 1.1, 190, BABYLON.Vector3.Zero(), scene);
   camera.attachControl(canvas, true);
-  // const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
-	// light.intensity = 0.7;
-	// light.diffuse = new BABYLON.Vector3(3,3,3)
+  //const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
+	//light.intensity = 0.7;
+	//light.diffuse = new BABYLON.Vector3(3,3,3)
 
 	createTemplateSphere(scene)
 
 	const earth = new Earth(scene);
 
-  let trash = renderTrash(scene);
+  trash = renderTrash(scene);
 
   scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
@@ -60,6 +63,7 @@ export function createScene (engine, canvas) {
 };
 
 export function updateScene() {
+  checkCollision(trash, 100);
 }
 
 function createTemplateSphere(scene){
