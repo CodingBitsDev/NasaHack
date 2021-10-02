6cd78114545b9ee8e7742e1826617f058ae3c441
Nils Rohr <nils.rohr@zreality.com>
2021-10-02 10:56:21 +0200

    create sphere

--- src/createScene.js
+++ src/createScene.js
@@ -3,27 +3,14 @@ export function createScene (engine, canvas) {
     let scene = new BABYLON.Scene(engine);
 
     // Add a camera to the scene and attach it to the canvas
-		var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-    // Add a lights to the scene
-		camera.setTarget(BABYLON.Vector3.Zero());
-
-		// This attaches the camera to the canvas
+		const camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
-
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
 
     // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+    // light.intensity = 0.7;
 
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-
-    // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+		const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {});
 
     //Your Code
 
